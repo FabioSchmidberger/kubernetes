116d5542f03b87dc8b58f634616983f087fb89b0
spethso <spethso@studi.informatik.uni-stuttgart.de>
2017-06-26 20:57:26 +0200

    Initial commit of simple NodeJS application

---
+++ application.js
@@ -0,0 +1,14 @@
+var Docker = require('dockerode');
+var http = require('http');
+
+var docker = new Docker({socketPath: '/var/run/docker.sock'});
+
+docker.listContainers(function (err, containers) {
+  containers.forEach(function (containerInfo) {
+    var containerID = docker.getContainer(containerInfo.Id);
+    console.log(containerID);
+    http.get('http://localhost:5050/blink/' + containerID, res => {
+        console.log('done');
+    });
+  });
+});
