85e5246c4d6c6ea8bcc552c9905a62de620d5502
Fabio Schmidberger <fabio@eduu.de>
2017-06-27 14:28:57 +0200

    js application

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
-FROM jsurf/rpi-raspbian
-RUN apt-get update && apt -y install curl
-CMD ["cat /proc/self/cgroup | grep "cpu:/" | sed 's/\([0-9]\):cpu:\/docker\///g'", "while true ; do curl http://localhost/blink/id & sleep 5; done"]
+FROM node:boron
+
+COPY application.js application.js
+RUN npm install http && npm install dockerode
+
+CMD ["node application.js"]
--- application.js
+++ application.js
@@ -1,14 +1,20 @@
-var Docker = require('dockerode');
+// var Docker = require('dockerode');
+
+
+// var docker = new Docker({socketPath: '/var/run/docker.sock'});
+//
+// docker.listContainers(function (err, containers) {
+//   containers.forEach(function (containerInfo) {
+//     var containerID = docker.getContainer(containerInfo.Id);
+//     console.log(containerID);
+//     http.get('http://localhost:5050/blink/' + containerID, res => {
+//         console.log('done');
+//     });
+//   });
+// });
+
 var http = require('http');
 
-var docker = new Docker({socketPath: '/var/run/docker.sock'});
-
-docker.listContainers(function (err, containers) {
-  containers.forEach(function (containerInfo) {
-    var containerID = docker.getContainer(containerInfo.Id);
-    console.log(containerID);
-    http.get('http://localhost:5050/blink/' + containerID, res => {
+http.get('http://goolge.com', res => {
         console.log('done');
-    });
-  });
 });
